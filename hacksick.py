a3a21479c5488ad77e7759de0c677450f790b23b
kirimine170 <kirimine170@gmail.com>
2024-10-30 21:38:02 +0900

    :pencil2: update hacksick.py index.html
    フォーム使えるように改修

--- hacksick.py
+++ hacksick.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import CSRFProtect
+from ip_form import IPForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hacksick' #サンプルだよ！ちゃんと変えなさいね！
+csrf = CSRFProtect(app)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    form = IPForm()
+    if form.validate_on_submit():
+        ip_address = form.ip_address.data
+        # TODO 処理書く
+    return render_template('index.html', form=form)
